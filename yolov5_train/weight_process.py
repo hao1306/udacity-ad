09c9f67d9baf21987abe0debfba857cd48a3d5fd
hao1306 <haoyang1306@outlook.com>
2023-09-06 19:26:58 +0200

    clear

--- yolov5_train/weight_process.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-ckpt = torch.load("yolov5-master/runs/train/exp4/weights/best.pt", map_location = "cpu")
-print(ckpt["model"])
-csd = ckpt["model"].float().state_dict()
-torch.save(csd,"yolov5-master/runs/train/exp4/weights/best_02.pt")
